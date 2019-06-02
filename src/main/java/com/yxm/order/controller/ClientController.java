fe715d0729dad66d5a221ed052852811fe7ea871
caesaryang <caesaryang06@163.com>
2019-06-02 15:30:44 +0800

    自定义负载均衡策略

--- src/main/java/com/yxm/order/controller/ClientController.java
+++ src/main/java/com/yxm/order/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.yxm.order.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +10,11 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ClientController {
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    private LoadBalancerClient loadBalancerClient;
 
 
 
@@ -19,8 +24,14 @@ public class ClientController {
 //        RestTemplate restTemplate = new RestTemplate();
 //        String result = restTemplate.getForObject("http://localhost:8080/msg", String.class);
 
-//        第二种调用方式
-        String result = restTemplate.getForObject("http://PRODUCT/msg", String.class);
+
+//        第二种方式
+        RestTemplate restTemplate = new RestTemplate();
+        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
+        String url = String.format("http://%s:%s/msg",serviceInstance.getHost(),serviceInstance.getPort());
+        String result = restTemplate.getForObject(url, String.class);
+//        第三种调用方式
+//        String result = restTemplate.getForObject("http://PRODUCT/msg", String.class);
         return  result;
     }
 }
